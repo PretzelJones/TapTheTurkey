024d3b40807dc1ab3b9a0da2b00eec3d6825d642
Sean Patterson <sean@bosson.design>
2017-11-15 18:21:42 -0500

    Initial Commit

---
+++ app/src/main/java/com/bossondesign/taptheturkey/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bossondesign.taptheturkey
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.TranslateAnimation
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val turkey = findViewById<ImageView>(R.id.turkey) as ImageView
+        val mp = MediaPlayer.create(this, R.raw.gobble)
+
+        turkey.setOnClickListener(View.OnClickListener {
+
+            turkey.startAnimation(TranslateAnimation(0f, 20f, 0f, 20f).apply {
+                duration = 10
+
+                repeatCount = 15
+                onPause()
+                mp!!.start()
+
+            })
+
+
+        })
+
+
+    }
+}
