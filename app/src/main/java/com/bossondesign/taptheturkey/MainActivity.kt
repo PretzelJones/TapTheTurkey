56df66186723c16d8f0ed7c8930812db3d26abf1
Sean Patterson <pretzeljones@icloud.com>
2025-07-18 13:30:03 -0400

    Updated to API 35, androidx, and java 17

--- app/src/main/java/com/bossondesign/taptheturkey/MainActivity.kt
+++ app/src/main/java/com/bossondesign/taptheturkey/MainActivity.kt
@@ -2,13 +2,15 @@ package com.bossondesign.taptheturkey
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
+import android.text.method.LinkMovementMethod
 import android.view.animation.TranslateAnimation
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +19,24 @@ class MainActivity : AppCompatActivity() {
         val turkey = findViewById<ImageView>(R.id.turkey) as ImageView
         val mp = MediaPlayer.create(this, R.raw.gobble)
 
+        //privacy policy text link
+        val textView = findViewById<TextView>(R.id.textPrivacy)
+        textView.movementMethod = LinkMovementMethod.getInstance()
+
         turkey.setOnClickListener {
             
             //handles animation
             turkey.startAnimation(TranslateAnimation(0f, 20f, 0f, 20f).apply {
                 duration = 10
                 repeatCount = 20
-                
+
                 //plays sound
                 onPause()
                 mp!!.start()
 
             })
 
-
         }
-
 
     }
 }
